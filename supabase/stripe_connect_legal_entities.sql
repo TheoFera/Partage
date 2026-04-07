1bf2d7adb9fa76ae75699c60b3ef6702ac00d6f2
TheoFera <feratheo@orange.fr>
2026-04-07 18:22:00 +0200

    Passage en prod

--- supabase/stripe_connect_legal_entities.sql
+++
@@ -1,7 +0,0 @@
-alter table public.legal_entities
-  add column if not exists stripe_account_id text,
-  add column if not exists stripe_account_country text;
-
-create unique index if not exists legal_entities_stripe_account_id_uidx
-  on public.legal_entities (stripe_account_id)
-  where stripe_account_id is not null;
